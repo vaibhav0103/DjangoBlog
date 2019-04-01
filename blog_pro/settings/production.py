6cc32df796d56df49afa5df34d04f87b5eb434c4
vaibhav <vaibhavsharma1393@gmail.com>
2019-04-01 17:05:10 +0530

    fix

--- blog_pro/settings/production.py
+++ blog_pro/settings/production.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from blog_pro.keys import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
