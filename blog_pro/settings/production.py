898ffb9b56b7f0d6310e99d606fbf121b802acb1
vaibhav <vaibhavsharma1393@gmail.com>
2019-04-01 17:44:36 +0530

    fix

--- blog_pro/settings/production.py
+++ blog_pro/settings/production.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY_DJANGO')
+SECRET_KEY = os.environ['SECRET_KEY_DJANGO']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
